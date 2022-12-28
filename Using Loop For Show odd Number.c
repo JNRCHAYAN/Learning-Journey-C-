6330d5e31bbf8cb3917761870546446aa722688b
Jogendra nath roy chayan <jogendranathroy40@gmail.com>
2022-12-28 23:41:30 +0600

    Update Using Loop For Show odd Number.c

--- Using Loop For Show odd Number.c
+++ Using Loop For Show odd Number.c
@@ -5,10 +5,29 @@ main()
     int i,n;
     printf("Entry your number: ");
     scanf("%d",&n);
+    i=1;
 
-    for (i=1;i<=n;i=i+2)
+   /* for (i=1;i<=n;i=i+2)
     {
         printf("The odd number: %d \n",i);
     }
+    return 0; */
+
+
+   /*  while(i<=n)
+    {
+        printf("The odd number: %d \n",i);
+
+        i=i+2;
+    }*/
+
+    do
+    {
+       printf("The odd number: %d \n",i);
+
+       i=i+2;
+    }
+    while(i<=n);
+
     return 0;
 }

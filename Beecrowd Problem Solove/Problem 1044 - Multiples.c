513ab48a1fa7c0ce0b4fa18a2b08f9bd646d478f
Jogendra nath roy chayan <jogendranathroy40@gmail.com>
2023-01-08 12:50:42 +0600

    Create Problem 1044 - Multiples.c

---
+++ Beecrowd Problem Solove/Problem 1044 - Multiples.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+    int A,B;
+    scanf("%d%d",&A,&B);
+
+    if (B%A==0)
+    {
+        printf("Sao Multiplos\n");
+    }
+    else
+    {
+        printf("Nao sao Multiplos\n");
+    }
+
+}

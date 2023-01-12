6cb4aa4460f5ffb8880d8e6db46843610ffa48f5
Jogendra nath roy chayan <jogendranathroy40@gmail.com>
2023-01-12 22:46:58 +0600

    Create Problem - 1155 - S Sequence.c

---
+++ Beecrowd Problem Solove/Problem - 1155 - S Sequence.c
@@ -0,0 +1,23 @@
+// Typer casting
+
+
+#include<stdio.h>
+
+main()
+{
+    int i;
+    float sum;
+
+
+    for (i=1;i<=100;i++)
+    {
+
+        sum=sum +(float)1/i;
+
+
+
+    }
+
+   printf("%.2f\n",sum);
+
+}

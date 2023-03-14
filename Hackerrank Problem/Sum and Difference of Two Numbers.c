a33e24315deb7ed127dd1d4e893bdd93d483838f
Jogendra nath roy chayan <jogendranathroy40@gmail.com>
2023-03-14 20:49:39 +0600

    Create Sum and Difference of Two Numbers.c

---
+++ Hackerrank Problem/Sum and Difference of Two Numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int a,b;
+    float c,d;
+    scanf("%d%d",&a,&b);
+ scanf("%f%f",&c,&d);
+    printf("%d ",(a+b));
+      printf("%d\n",(a-b));
+        printf("%.1f ",(c+d));
+          printf("%.1f\n",(c-d));
+}
